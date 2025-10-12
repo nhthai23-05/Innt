a451307fa78a753c6d9ca34e372f5d5a15a25ae5
nhthai23-05 <hoangthai.nguyen.0203@gmail.com>
2025-10-12 17:02:50 +0700

    Fix config

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+  phoneNumber: import.meta.env.VITE_PHONE_NUMBER || '',
+  email: import.meta.env.VITE_EMAIL || '',
+  address: import.meta.env.VITE_ADDRESS || '',
+  contactUrl: import.meta.env.VITE_CONTACT_URL || '',
+};
